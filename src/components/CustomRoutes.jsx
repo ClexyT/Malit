064bc8080f9ea93da18efa52c5535785f44b4950
ClexyT <zombirt67@gmail.com>
2025-09-17 11:44:05 -0600

    fix(router): mount HashRouter at root

--- src/components/CustomRoutes.jsx
+++ src/components/CustomRoutes.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import SOON from '../SOON'
 import App from '../App'
 
 export default function CustomRoutes() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<SOON />} />
-        <Route path="/experimental_features_malit" element={<App />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<SOON />} />
+      <Route path="/experimental_features_malit" element={<App />} />
+    </Routes>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import CustomRoutes from './components/CustomRoutes'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CustomRoutes />
+    <HashRouter>
+      <CustomRoutes />
+    </HashRouter>
   </React.StrictMode>
 )
