727f55138596052e6c29c1547d9fe8793b55b7a3
ClexyT <zombirt67@gmail.com>
2025-09-17 11:10:33 -0600

    17-09-25 1

--- src/components/CustomRoutes.jsx
+++ src/components/CustomRoutes.jsx
@@ -1,16 +1,15 @@
-
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import SOON from '../SOON'
 import App from '../App'
 
 export default function CustomRoutes() {
   return (
-    <BrowserRouter basename="/Malit">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<SOON />} />
         <Route path="/experimental_features_malit" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
